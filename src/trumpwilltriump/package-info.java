05571a8709baa8a4f0e78ba20964c5a914faff95
Bailey Thompson <bailey.kyle.thompson@gmail.com>
2017-05-06 16:19:01 -0400

    Version 1.4.5
    Clarify Jeopardy question, and accept multiple spellings of an answer.

--- src/trumpwilltriump/package-info.java
+++ src/trumpwilltriump/package-info.java
@@ -1,7 +1,7 @@
 /**
  * Bailey Thompson <br>
- * Trump Will Triump (1.4.4) <br>
- * 18 April 2017 <br>
+ * Trump Will Triump (1.4.5) <br>
+ * 6 May 2017 <br>
  * <br>
  * You must play as Donald Trump to conquer the USA a state at a time by playing various mini-games.&nbsp;Each state has
  * its own mini-game.&nbsp;If the mini-game is won, the state becomes Republican, and thus red.&nbsp;If the mini-game is
